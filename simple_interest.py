c780a69ef4f52e0b2fa305e856bf7cf3af7bcec9
aminubello <aminubellz249@gmail.com>
2025-07-07 12:43:25 +0100

    Added simple_interest.py file.

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000 #this is the principal for the interest
+rate = 0.05 #representing 5% annual interest rate
+time = 3 #representing 3 years
+
+interest = principal * rate * time
+print("The simple interest is: ", interest)
